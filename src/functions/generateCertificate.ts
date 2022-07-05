d5490dcd5513c75e853ced7a5e0f69922d9db024
neanderdev <neander.faria@hotmail.com>
2022-07-05 15:46:42 -0300

    Creating template

--- src/functions/generateCertificate.ts
+++ src/functions/generateCertificate.ts
@@ -1,4 +1,8 @@
 import { APIGatewayProxyHandler } from "aws-lambda";
+import { compile } from "handlebars";
+import { join } from "path";
+import { readFileSync } from "fs";
+import * as dayjs from "dayjs";
 
 import { document } from "../utils/dynamodbClient";
 
@@ -7,6 +11,22 @@ interface ICreateCertificate {
   name: string;
   grade: string;
 }
+
+interface ITemplate {
+  id: string;
+  name: string;
+  grade: string;
+  medal: string;
+  date: string;
+}
+
+const compileTemplate = async (data: ITemplate) => {
+  const filePath = join(process.cwd(), "src", "templates", "certificate.hbs");
+
+  const html = readFileSync(filePath, "utf-8");
+
+  return compile(html)(data);
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
   const { id, name, grade } = JSON.parse(event.body) as ICreateCertificate;
@@ -22,6 +42,19 @@ export const handler: APIGatewayProxyHandler = async (event) => {
       },
     })
     .promise();
+
+  const medalPath = join(process.cwd(), "src", "templates", "selo.png");
+  const medal = readFileSync(medalPath, "base64");
+
+  const data: ITemplate = {
+    id,
+    name,
+    grade,
+    date: dayjs().format("DD/MM/YYYY"),
+    medal,
+  };
+
+  const content = await compileTemplate(data);
 
   const response = await document
     .query({
